347a5da0a0b659accb91f92d0acbd9aa086da2bc
anonymoussc <50c5ac69@opayq.com>
2016-02-28 23:58:53 +0700

    Add create@ groups

---
+++ resources/angular-material/app/dashboard/groups/create/create.controller.js
@@ -0,0 +1,29 @@
+/**
+ * create.controller.js
+ * Created by anonymous on 29/12/15 21:02.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('dashboard')
+        .controller('GroupsCreateController', GroupsCreateController);
+
+    GroupsCreateController.$inject = [];
+
+    /* @ngInject */
+    function GroupsCreateController() {
+        var vm = this;
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+
