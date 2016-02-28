28823a4118170125acb26ea5af0fd32672bb7c2f
anonymoussc <50c5ac69@opayq.com>
2016-02-28 23:55:38 +0700

    Add edit-password@ users

---
+++ resources/angular-material/app/dashboard/users/edit-password/edit-password.controller.js
@@ -0,0 +1,33 @@
+/**
+ * edit-password.controller.js
+ * Created by anonymous on 21/02/16 23:22.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('dashboard')
+        .controller('UsersEditPasswordController', UsersEditPasswordController);
+
+    UsersEditPasswordController.$inject = ['$stateParams'];
+
+    /* @ngInject */
+    function UsersEditPasswordController($stateParams) {
+
+        var vm = this;
+
+        vm.data = {id: $stateParams.id};
+
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+
