f4dbf92148111a5b2a825792af88e56f03fc91df
anonymoussc <50c5ac69@opayq.com>
2016-02-28 13:12:17 +0700

    Add sidenav

---
+++ resources/angular-material/app/dashboard/sidenav/sidenav.controller.js
@@ -0,0 +1,35 @@
+/**
+ * sidenav.controller.js
+ * Created by anonymous on 03/01/16 4:38.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('dashboard')
+        .controller('DashboardSidenavController', DashboardSidenavController);
+
+    DashboardSidenavController.$inject = ['$timeout', '$mdSidenav', '$log'];
+
+    /* @ngInject */
+    function DashboardSidenavController($timeout, $mdSidenav, $log) {
+        var vm = this;
+
+        vm.close = function() {
+            $mdSidenav('left').close().then(function() {
+                // $log.debug("close LEFT is done");
+            });
+        };
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+
