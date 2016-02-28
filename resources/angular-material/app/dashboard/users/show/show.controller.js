ef34765f19ec202c8ced03c173f8ff7cc7e6b6e4
anonymoussc <50c5ac69@opayq.com>
2016-02-28 23:54:44 +0700

    Add show@ users

---
+++ resources/angular-material/app/dashboard/users/show/show.controller.js
@@ -0,0 +1,30 @@
+/**
+ * show.controller.js
+ * Created by anonymous on 16/12/15 14:38.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('dashboard')
+        .controller('UsersShowController', UsersShowController);
+
+    UsersShowController.$inject = ['$stateParams', 'Users'];
+
+    /* @ngInject */
+    function UsersShowController($stateParams, Users) {
+        var vm  = this;
+        vm.data = {id: $stateParams.id};
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+
