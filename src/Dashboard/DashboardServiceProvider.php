22ed250a5472649af211b14cd0f9ec944b9f7f12
anonymoussc <50c5ac69@opayq.com>
2016-02-29 04:32:08 +0700

    Updated ServiceProvider

--- src/Dashboard/DashboardServiceProvider.php
+++ src/Dashboard/DashboardServiceProvider.php
@@ -37,6 +37,7 @@ class DashboardServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->register(\Onderdelen\AppFoundation\AppFoundationServiceProvider::class);
+        $this->app->register(\Onderdelen\Seneschal\SeneschalServiceProvider::class);
     }
 
     /**
