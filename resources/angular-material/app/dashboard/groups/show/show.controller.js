8c984dc3ecbb3060aa3292f3818a3726dfe1f0eb
anonymoussc <50c5ac69@opayq.com>
2016-02-28 23:57:36 +0700

    Add show@ groups

---
+++ resources/angular-material/app/dashboard/groups/show/show.controller.js
@@ -0,0 +1,30 @@
+/**
+ * show.controller.js
+ * Created by anonymous on 29/12/15 21:07.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('dashboard')
+        .controller('GroupsShowController', GroupsShowController);
+
+    GroupsShowController.$inject = ['$stateParams', 'Groups'];
+
+    /* @ngInject */
+    function GroupsShowController($stateParams, Groups) {
+        var vm  = this;
+        vm.data = {id: $stateParams.id};
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+
