d994ad6d7337c8949e005e81289f2a76d66b2295
anonymoussc <50c5ac69@opayq.com>
2016-02-28 23:33:49 +0700

    Modified states

--- resources/angular-material/states.js
+++ resources/angular-material/states.js
@@ -10,11 +10,12 @@
         .module('dashboard')
         .config(dashboardRouter);
 
-    dashboardRouter.$inject = ['$stateProvider', '$urlRouterProvider', 'layoutProvider'];
+    dashboardRouter.$inject = ['$stateProvider', '$urlRouterProvider', '$httpProvider', '$provide', 'layoutProvider'];
 
     /* @ngInject */
-    function dashboardRouter($stateProvider, $urlRouterProvider, layoutProvider) {
-
+    function dashboardRouter($stateProvider, $urlRouterProvider, $httpProvider, $provide, layoutProvider) {
+        $provide.factory('redirectWhenLoggedOut', redirectWhenLoggedOut);
+        $httpProvider.interceptors.push('redirectWhenLoggedOut');
         $urlRouterProvider.otherwise('/');
 
         $stateProvider
@@ -54,13 +55,169 @@
                 url  : '/',
                 data : {pageName: 'Overview'},
                 views: {
-                    'main@dashboard'   : {
+                    'main@dashboard': {
                         templateUrl : layoutProvider.view('dashboard.home'),
                         controller  : 'DashboardHomeController',
                         controllerAs: 'home'
                     }
                 }
+            })
+            .state('dashboard.groups', {
+                url  : '/groups',
+                data : {pageName: 'Groups'},
+                views: {
+                    'main@dashboard': {
+                        templateUrl : layoutProvider.dashboard('groups.index'),
+                        controller  : 'GroupsIndexController',
+                        controllerAs: 'index'
+                    }
+                }
+            })
+            .state('dashboard.groups.create', {
+                url  : '/create',
+                data : {pageName: 'Create'},
+                views: {
+                    'main@dashboard': {
+                        templateUrl : layoutProvider.dashboard('groups.create'),
+                        controller  : 'GroupsCreateController',
+                        controllerAs: 'create'
+                    }
+                }
+            })
+            .state('dashboard.groups.show', {
+                url  : '/show/{id}',
+                data : {pageName: 'Show'},
+                views: {
+                    'main@dashboard': {
+                        templateUrl : layoutProvider.dashboard('groups.show'),
+                        controller  : 'GroupsShowController',
+                        controllerAs: 'show'
+                    }
+                }
+            })
+            .state('dashboard.groups.edit', {
+                url  : '/edit/{id}',
+                data : {pageName: 'Edit'},
+                views: {
+                    'main@dashboard': {
+                        templateUrl : layoutProvider.dashboard('groups.edit'),
+                        controller  : 'GroupsEditController',
+                        controllerAs: 'edit'
+                    }
+                }
+            })
+            .state('dashboard.users', {
+                url  : '/users',
+                data : {pageName: 'Users'},
+                views: {
+                    'main@dashboard': {
+                        templateUrl : layoutProvider.dashboard('users.index'),
+                        controller  : 'UsersIndexController',
+                        controllerAs: 'index'
+                    }
+                }
+            })
+            .state('dashboard.users.create', {
+                url  : '/create',
+                data : {pageName: 'Create'},
+                views: {
+                    'main@dashboard': {
+                        templateUrl : layoutProvider.dashboard('users.create'),
+                        controller  : 'UsersCreateController',
+                        controllerAs: 'create'
+                    }
+                }
+            })
+            .state('dashboard.users.show', {
+                url  : '/show/{id}',
+                data : {pageName: 'Show'},
+                views: {
+                    'main@dashboard': {
+                        templateUrl : layoutProvider.dashboard('users.show'),
+                        controller  : 'UsersShowController',
+                        controllerAs: 'show'
+                    }
+                }
+            })
+            .state('dashboard.users.edit', {
+                url  : '/edit/{id}',
+                data : {pageName: 'Edit'},
+                views: {
+                    'main@dashboard': {
+                        templateUrl : layoutProvider.dashboard('users.edit'),
+                        controller  : 'UsersEditController',
+                        controllerAs: 'edit'
+                    }
+                }
+            })
+            .state('dashboard.users.edit-membership', {
+                url  : '/edit-membership/{id}',
+                data : {pageName: 'Edit Membership'},
+                views: {
+                    'main@dashboard': {
+                        templateUrl : layoutProvider.dashboard('users.edit-membership'),
+                        controller  : 'UsersEditMembershipController',
+                        controllerAs: 'edit'
+                    }
+                }
+            })
+            .state('dashboard.users.edit-password', {
+                url  : '/edit-password/{id}',
+                data : {pageName: 'Edit Password'},
+                views: {
+                    'main@dashboard': {
+                        templateUrl : layoutProvider.dashboard('users.edit-password'),
+                        controller  : 'UsersEditPasswordController',
+                        controllerAs: 'edit'
+                    }
+                }
             });
+
+        function redirectWhenLoggedOut($q, $injector) {
+
+            var respError = {
+                responseError: responseError
+            };
+
+            function responseError(rejection) {
+                var
+                    $state           = $injector.get('$state'),
+                    rejectionReasons = ['token_not_provided', 'token_expired', 'token_absent', 'token_invalid'];
+
+                angular.forEach(rejectionReasons, function(value, key) {
+                    if (rejection.data.error === value) {
+                        localStorage.removeItem('user');
+                        $state.go('jwtauth.signin');
+                    }
+                });
+
+                return $q.reject(rejection);
+            }
+
+            return respError;
+        }
+
+        function skipIfLoggedIn($q, $auth) {
+            var deferred = $q.defer();
+            if ($auth.isAuthenticated()) {
+                deferred.reject();
+            } else {
+                deferred.resolve();
+            }
+
+            return deferred.promise;
+        }
+
+        function loginRequired($q, $state, $auth) {
+            var deferred = $q.defer();
+            if ($auth.isAuthenticated()) {
+                deferred.resolve();
+            } else {
+                $state.go('jwtauth.signin');
+            }
+
+            return deferred.promise;
+        }
 
     }
 
