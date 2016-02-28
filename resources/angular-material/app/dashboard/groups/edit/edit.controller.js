7fddc1297b3f9ce6a6ebaa3e3bf16a8f3a48da93
anonymoussc <50c5ac69@opayq.com>
2016-02-28 23:58:25 +0700

    Add edit@ groups

---
+++ resources/angular-material/app/dashboard/groups/edit/edit.controller.js
@@ -0,0 +1,30 @@
+/**
+ * edit.controller.js
+ * Created by anonymous on 29/12/15 21:09.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('dashboard')
+        .controller('GroupsEditController', GroupsEditController);
+
+    GroupsEditController.$inject = ['$stateParams', 'Groups'];
+
+    /* @ngInject */
+    function GroupsEditController($stateParams, Groups) {
+        var vm  = this;
+        vm.data = {id: $stateParams.id};
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+
